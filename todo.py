1e46253c245b1ac42137bf9217267bdb65a686ef
“zhangnblh” <2551136323@qq.com>
2026-08-14 13:53:01 +0800

    feat: add task listing (#3)

--- todo.py
+++ todo.py
@@ -9,9 +9,17 @@ def add_task(title):
     tasks.append(task)
 
 
+def list_tasks():
+    for index, task in enumerate(tasks, start=1):
+        status = "Done" if task["completed"] else "Todo"
+        print(f"{index}. [{status}] {task['title']}")
+
+
 def main():
     add_task("Learn GitHub")
-    print(tasks)
+    add_task("Learn IntelliJ IDEA")
+
+    list_tasks()
 
 
 if __name__ == "__main__":
